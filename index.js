d0b45e43c4d32bccfb72f279666da517c1ab954d
Alam Hosen <alamhosen.web@gmail.com>
2022-05-15 20:28:19 +0600

    specific user data loaded in dashboard

--- index.js
+++ index.js
@@ -60,6 +60,14 @@ async function run() {
      * app.patch('/booking/:id) //
      * app.delete('/booking/:id) //
     */
+
+         app.get('/booking', async(req, res) =>{
+            const patient = req.query.patient;
+            const query = {patient: patient};
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+          })
+
         app.post('/booking', async (req, res) => {
             const booking = req.body;
             const query = { treatment: booking.treatment, date: booking.date, pathent: booking.pathent }
